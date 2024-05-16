553f2ea016d2e55e0685f689bf5c33d422fc02d0
bm5apa <timelessheaven@gmail.com>
2024-05-16 21:16:37 +0800

    build: making generator

--- app.js
+++ app.js
@@ -9,9 +9,46 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const checkValue = req.query.checkboxes || []
+  const passwordlength = req.query.passwordlength
+  let password = generatePassword(checkValue, passwordlength)
+  res.render('index', { passwordlength , password })
 })
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
+
+
+function generatePassword(checkboxes, passwordlength){
+
+let password = ''
+let characterArr = []
+const characterLower = 'abcdefghijklmnopqrstuvwxyz'
+const characterUpper = characterLower.toUpperCase()
+const numbers = '1234567890'
+const specialIcon = `!@#$%^&*(){}"?><`
+
+if(checkboxes.includes('checkbox1')){
+characterArr = characterArr.concat(characterLower.split(''))
+};
+
+if(checkboxes.includes('checkbox2')){
+characterArr = characterArr.concat(characterUpper.split(''))
+}
+
+if(checkboxes.includes('checkbox3')){
+characterArr = characterArr.concat(numbers.split(''))
+}
+
+if(checkboxes.includes('checkbox4')){
+characterArr = characterArr.concat(specialIcon.split(''))
+}
+
+for(let i = 1; i <= passwordlength; i++){
+password += characterArr[Math.floor(Math.random()*(characterArr.length))]
+}
+
+console.log('password', password)
+  return password
+}
